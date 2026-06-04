5e64b28bb7cad72f9f7ee367ab0060f77e1ff849
Yewon Admin <admin@yewon.ac.kr>
2026-06-05 08:54:06 +0900

    chore: setup opennext configuration

--- src/app/admin/rules/[id]/edit/page.tsx
+++ src/app/admin/rules/[id]/edit/page.tsx
@@ -1,7 +1,5 @@
 import EditClient from "./EditClient";
 
-export const runtime = "edge";
-
 export default async function RuleEditPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return <EditClient id={id} />;
