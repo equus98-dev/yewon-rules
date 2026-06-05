861aac81f5fa7c047607f63fe5cd969f4913077a
Yewon Admin <admin@yewon.ac.kr>
2026-06-05 15:53:53 +0900

    fix: correctly patch opennext esbuild config for v1.x

--- patch-opennext.js
+++ patch-opennext.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = 'node_modules/@opennextjs/cloudflare/dist/cli/build/open-next/createServerBundle.js';
 let code = fs.readFileSync(path, 'utf8');
-if (!code.includes('keepNames')) {
-  code = code.replace('minify: !options.noMinify,', 'minify: !options.noMinify, keepNames: true,');
+
+if (code.includes('minify: options.minify,')) {
+  code = code.replace('minify: options.minify,', 'minifyIdentifiers: false, minifySyntax: options.minify, minifyWhitespace: options.minify, keepNames: true,');
   fs.writeFileSync(path, code);
-  console.log('Patched OpenNext esbuild config to keepNames');
+  console.log('Patched OpenNext esbuild config to avoid minifying identifiers!');
+} else {
+  console.log('Warning: Could not find "minify: options.minify," to patch in OpenNext');
 }
