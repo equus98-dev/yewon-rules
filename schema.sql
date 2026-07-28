f84aa874dcc3f1e946b5bb354ac514ff0a5976c2
Yewon Admin <admin@yewon.ac.kr>
2026-07-28 15:36:22 +0900

    chore: add indexes to prevent full table scans and reduce rows read

--- schema.sql
+++ schema.sql
@@ -101,6 +101,13 @@ CREATE TABLE "Notice" (
   "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX IF NOT EXISTS "idx_Article_revisionId" ON "Article"("revisionId");
+CREATE INDEX IF NOT EXISTS "idx_Revision_ruleId" ON "Revision"("ruleId");
+CREATE INDEX IF NOT EXISTS "idx_Rule_categoryId" ON "Rule"("categoryId");
+CREATE INDEX IF NOT EXISTS "idx_Rule_departmentId" ON "Rule"("departmentId");
+CREATE INDEX IF NOT EXISTS "idx_Attachment_ruleId" ON "Attachment"("ruleId");
+CREATE INDEX IF NOT EXISTS "idx_ArticleComparison_revisionId" ON "ArticleComparison"("revisionId");
  
 CREATE TABLE " "Opinion ( 
   id TEXT NOT NULL PRIMARY KEY, 
